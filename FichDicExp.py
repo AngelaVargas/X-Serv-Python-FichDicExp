4f46f5dd53c558b8264bcdd94c617488375e1b25
Angela <AngelaVargas>
2017-02-07 00:07:27 +0100

    FicherosDiccionariosExcepciones

---
+++ FichDicExp.py
@@ -0,0 +1,17 @@
+
+fich = open ("/etc/passwd","r");
+Lineas = fich.readlines();
+dic = {}
+
+for Linea in Lineas:
+	usuario = Linea.split(':')
+	dic[usuario[0]] = {usuario[6]}
+	
+try:
+	print (dic["root"])
+	print (dic["imaginario"])
+
+except:			#Si no lo encuentra:
+	print ("Ese usuario no existe")
+
+fich.close()
